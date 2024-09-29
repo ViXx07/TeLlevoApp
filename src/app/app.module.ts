dc75b28df19627ab3fe33dee453b75a8a005f2bd
CarolPachecoPozo <caro.pacheco@gmail.com>
2024-09-28 23:36:05 -0300

    conexion firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { environment } from 'src/environments/environment';
+import { AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import { AngularFireModule} from '@angular/fire/compat';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+	  AngularFirestoreModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,28 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAifg4bzLuB2bYpC_Quei-Kq2zssOfD9Tk",
+    authDomain: "tellevoappbd-grupo1.firebaseapp.com",
+    projectId: "tellevoappbd-grupo1",
+    storageBucket: "tellevoappbd-grupo1.appspot.com",
+    messagingSenderId: "1056043870158",
+    appId: "1:1056043870158:web:cdc4c7d44ed9b1ce5e6d9f",
+    measurementId: "G-7FGTTHMXNJ"
+  },
 };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAifg4bzLuB2bYpC_Quei-Kq2zssOfD9Tk",
+  authDomain: "tellevoappbd-grupo1.firebaseapp.com",
+  projectId: "tellevoappbd-grupo1",
+  storageBucket: "tellevoappbd-grupo1.appspot.com",
+  messagingSenderId: "1056043870158",
+  appId: "1:1056043870158:web:cdc4c7d44ed9b1ce5e6d9f",
+  measurementId: "G-7FGTTHMXNJ"
+};
+
 
 /*
  * For easier debugging in development mode, you can import the following file
