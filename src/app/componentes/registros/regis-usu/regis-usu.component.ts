9a3531582ccc469f0a7b69eb90b8a03ff142dfe4
joaquin <jo.perezu@duocuc.cl>
2024-10-03 16:01:23 -0300

    Validaciones pasajero, grabado 100% funcional

--- src/app/componentes/registros/regis-usu/regis-usu.component.ts
+++ src/app/componentes/registros/regis-usu/regis-usu.component.ts
@@ -10,10 +10,6 @@ import { CrudPasajeroService } from 'src/app/servicio/pasajero/crud-pasajero.ser
 })
 export class RegisUsuComponent  implements OnInit {
 
-  nombre            : string = '';
-  apellido          : string = '';
-  rut               : string = '';
-  genero            : string = '';
   mostrarPaso       : number = 1 ;
   contrasena2: string = '';
   
@@ -30,22 +26,45 @@ export class RegisUsuComponent  implements OnInit {
   };
   
   /* bool manejo imputs */
-  errNombre      : boolean = false;
-  errApellido    : boolean = false;
-  errRut         : boolean = false;
-  errGenero      : boolean = false;
-  errCorreo      : boolean = false;
-  errNacimiento  : boolean = false;
-  errContrasena  : boolean = false;
-  errContrasena2 : boolean = false;
-  vNombre(){this.errNombre = this.usuario.nombre === '';}
-  vApellido(){this.errApellido = this.usuario.apellido === ''}
-  vRut   (){this.errRut    = this.usuario.rut    === ''}
-  vGenero(){this.errGenero = this.usuario.genero === ''}
-  vCorreo(){this.errCorreo = this.usuario.correoElectronico === ''}
+  errNombre      : boolean | null = null;
+  errApellido    : boolean | null = null;
+  errRut         : boolean | null = null;
+  errGenero      : boolean | null = null;
+  errCorreo      : boolean | null = null;
+  errNacimiento  : boolean | null = null;
+  errContrasena  : boolean | null = null;
+
+  vNombre  () {this.errNombre   = this.usuario.nombre   === '';};
+  vApellido() {this.errApellido = this.usuario.apellido === '';};
+  vRut   (){
+    const regex = /^[0-9]{7,8}[0-9kK]$/i;
+    const validar = regex.test(this.usuario.rut);
+    if (validar) {
+      return this.errRut=false;
+    } else {
+      return this.errRut=true;
+    }
+  };
+  vGenero(){this.errGenero = this.usuario.genero === ''};
+  vCorreo(){
+    const regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    const validar = regex.test(this.usuario.correoElectronico);
+    if (validar) {
+      return this.errCorreo=false;
+    } else {
+      return this.errCorreo=true;
+    }
+  };
   /* vNacimiento(){this.errNacimiento = this.usuario.fecha_nac === ''} */
-  vContrasena(){this.errContrasena = this.usuario.contrasena === ''}
-  vcontrasena2(){this.errContrasena2 = this.contrasena2 === ''}
+  vContrasena() {
+    if (this.usuario.contrasena !== '' && this.contrasena2 !== '' && this.usuario.contrasena === this.contrasena2) {
+      this.errContrasena=false;
+      return true;
+    } else {
+      this.errContrasena=true;
+      return false;
+    };
+  };
   
   constructor(private navCtrl : NavController,
     private crudPasajero: CrudPasajeroService,
@@ -55,19 +74,35 @@ export class RegisUsuComponent  implements OnInit {
   goLogin(){
     this.navCtrl.navigateBack(['/login']);
   }
+
   validarPaso1(){
-    if (this.usuario.nombre != '' && this.usuario.apellido != '' && this.usuario.rut != '') {
+    /* Si todos los parametros dan que son '' daran true, por lo cual verifico que sean false para continuar */
+    if (this.errNombre===false && this.errApellido===false && this.errRut===false) {
+      /* Te manda al paso numero 2 */
       this.mostrarPaso = 2;
     } else {
+      /* ejecuta las 3 funciones para revisar si estan completadas y que salte el mensaje de error por estar vacio c: */
       this.vNombre();
       this.vApellido();
       this.vRut();
     } 
   }
   validarPaso2(){
-    this.mostrarPaso = 3;
+    if (this.errGenero === false && this.errCorreo === false) {
+      this.mostrarPaso = 3;
+    } else {
+      this.vGenero();
+      this.vCorreo();
+    }
   }
-
+  validarFinal(){
+    if (this.vContrasena()) {
+      this.grabar();
+    } else {
+      alert(this.errContrasena)
+      this.vContrasena();
+    }
+  }
 
   goPaso1(){
     if (this.mostrarPaso>1) {
