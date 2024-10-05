538c97e2fc62759e1dd4f15092122dd35b941ea5
CarolPachecoPozo <caro.pacheco@gmail.com>
2024-10-05 19:18:46 -0300

    crud registro viaje

--- src/app/Pages/chofer/crear-viaje/crear-viaje.page.ts
+++ src/app/Pages/chofer/crear-viaje/crear-viaje.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Viaje } from 'src/app/model/Viaje';
+import { CrudViajeService } from 'src/app/servicio/viaje/crud-viaje.service';
 
 @Component({
   selector: 'app-crear-viaje',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-viaje.page.scss'],
 })
 export class CrearViajePage implements OnInit {
+  viaje: Viaje = {destino:'', hora_inicio:0, punto_encuentro:'', valor:0}
 
-  constructor() { }
+  constructor(private crud:CrudViajeService) { }
 
   ngOnInit() {
   }
 
+  grabar(){
+    this.crud.grabar(this.viaje).then(()=>{
+      alert('Viaje registrado')
+    }).catch((err)=>{
+      console.log("error:", err)
+    })
+  }
 }
--- src/app/servicio/viaje/crud-viaje.service.ts
+++ src/app/servicio/viaje/crud-viaje.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Viaje } from 'src/app/model/Viaje';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class CrudViajeService {
 
-  constructor() { }
+  constructor(private afs:AngularFirestore) { }
+
+  grabar(viaje:Viaje){
+    return this.afs.collection('viaje').add(viaje);
+  }
+
+  listar(): Observable<Viaje[]>{
+    return this.afs.collection<Viaje>('viaje').valueChanges({idField: 'id'})
+  }
+
 }
