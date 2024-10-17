35ac07d96825b1ab681d3975485d9fbb529cb751
joaco <jo.perezu@duocuc.cl>
2024-10-17 12:53:36 -0300

    mapita

--- src/app/Pages/chofer/home-chofer/home-chofer.page.ts
+++ src/app/Pages/chofer/home-chofer/home-chofer.page.ts
@@ -12,25 +12,26 @@ import { environment } from 'src/environments/environment';
 export class HomeChoferPage implements OnInit {
   constructor(private navCtrl: NavController,) { }
 
-  ngOnInit():void {
+  ionViewWillEnter(){
     if (localStorage.getItem('perfil')==='pasajero') {
       this.navCtrl.navigateRoot('home-pasajero')
     }
     this.mapa() 
+  }
+  
+  ngOnInit():void {
+    
 
   }
 
   mapa(){
-    //var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
-    //mapboxgl.accessToken = environment.MAPBOX_ACCESS_TOKEN;
-    
     var map = new mapboxgl.Map({
 
       accessToken: environment.MAPBOX_ACCESS_TOKEN,
       container: 'mapa-box',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center:[-33.59846466294888, -70.57880159991507],
-      zoom:12,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center:[ -70.57880159991507,-33.59846466294888],
+      zoom:16,
       collectResourceTiming: false,
     });
   }
--- src/app/Pages/pasajero/home-pasajero/home-pasajero.page.ts
+++ src/app/Pages/pasajero/home-pasajero/home-pasajero.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+//Importaciones para el mapa 
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-home-pasajero',
@@ -10,10 +14,26 @@ export class HomePasajeroPage implements OnInit {
 
   constructor(private navCtrl: NavController,) { }
 
-  ngOnInit() {
+  ionViewWillEnter(){
     if (localStorage.getItem('perfil')==='chofer') {
       this.navCtrl.navigateRoot('home-chofer')
     }
+    this.mapa()
   }
 
+  ngOnInit() {
+    
+  }
+
+  mapa(){
+    var map = new mapboxgl.Map({
+
+      accessToken: environment.MAPBOX_ACCESS_TOKEN,
+      container: 'mapa-box',
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center:[ -70.57880159991507,-33.59846466294888],
+      zoom:16,
+      collectResourceTiming: false,
+    });
+  }
 }
