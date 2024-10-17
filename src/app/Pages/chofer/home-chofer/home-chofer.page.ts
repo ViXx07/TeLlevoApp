ed473b35e63c2cddc79b5fd6fe45a76a1d6b55e0
CarolPachecoPozo <caro.pacheco@gmail.com>
2024-10-17 00:45:01 -0300

    D:

--- src/app/Pages/chofer/home-chofer/home-chofer.page.ts
+++ src/app/Pages/chofer/home-chofer/home-chofer.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+//Importaciones para el mapa 
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home-chofer',
@@ -9,10 +12,27 @@ import { NavController } from '@ionic/angular';
 export class HomeChoferPage implements OnInit {
   constructor(private navCtrl: NavController,) { }
 
-  ngOnInit() {
+  ngOnInit():void {
     if (localStorage.getItem('perfil')==='pasajero') {
       this.navCtrl.navigateRoot('home-pasajero')
     }
+    this.mapa() 
+
+  }
+
+  mapa(){
+    //var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+    //mapboxgl.accessToken = environment.MAPBOX_ACCESS_TOKEN;
+    
+    var map = new mapboxgl.Map({
+
+      accessToken: environment.MAPBOX_ACCESS_TOKEN,
+      container: 'mapa-box',
+      style: 'mapbox://styles/mapbox/light-v10',
+      center:[-33.59846466294888, -70.57880159991507],
+      zoom:12,
+      collectResourceTiming: false,
+    });
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,7 @@
 
 export const environment = {
   production: false,
+  MAPBOX_ACCESS_TOKEN:'pk.eyJ1IjoianVha28xMDUiLCJhIjoiY20yY3F4MzFuMGxjZjJpbzh1MmtwN3RkeSJ9.eq0Klw01jvADGQMMR8THBw',
   firebaseConfig : {
     apiKey: "AIzaSyAifg4bzLuB2bYpC_Quei-Kq2zssOfD9Tk",
     authDomain: "tellevoappbd-grupo1.firebaseapp.com",
@@ -11,7 +12,8 @@ export const environment = {
     storageBucket: "tellevoappbd-grupo1.appspot.com",
     messagingSenderId: "1056043870158",
     appId: "1:1056043870158:web:cdc4c7d44ed9b1ce5e6d9f",
-    measurementId: "G-7FGTTHMXNJ"
+    measurementId: "G-7FGTTHMXNJ",
+    
   }
 };
 
