90ae34070e38465f14d9d78d3cbd899f04370e0d
Exeq-dev <albornozexequiel01@gmail.com>
2024-10-23 11:22:12 -0300

    xd

--- src/app/Pages/pasajero/home-pasajero/home-pasajero.page.ts
+++ src/app/Pages/pasajero/home-pasajero/home-pasajero.page.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
-import { Viaje, viajeVacio } from 'src/app/model/Viaje';
-import { CrudChoferService } from 'src/app/servicio/chofer/crud-chofer.service';
-import {CrudViajeService} from 'src/app/servicio/viaje/crud-viaje.service'
 //Importaciones para el mapa 
 import * as mapboxgl from 'mapbox-gl';
+import { Viaje, viajeVacio } from 'src/app/model/Viaje';
+import { CrudChoferService } from 'src/app/servicio/chofer/crud-chofer.service';
+import { CrudViajeService } from 'src/app/servicio/viaje/crud-viaje.service';
 import { environment } from 'src/environments/environment';
 
 
@@ -16,17 +16,53 @@ import { environment } from 'src/environments/environment';
 export class HomePasajeroPage implements OnInit {
 
   constructor(private navCtrl: NavController,
-              private crudViaje: CrudViajeService,
-              private crudChofer: CrudChoferService,
-  ) { }
+    private crudViaje: CrudViajeService,
+    private crudChofer: CrudChoferService,
 
-  viajes:Viaje[]=[]
-  idPasajero=localStorage.getItem('idUsuario')
+  ) { }
 
   ionViewWillEnter(){
     if (localStorage.getItem('perfil')==='chofer') {
       this.navCtrl.navigateRoot('home-chofer')
     }
+    this.mapa()
   }
 
+
+  mapa(){
+    var map = new mapboxgl.Map({
+
+      accessToken: environment.MAPBOX_ACCESS_TOKEN,
+      container: 'mapa-box',
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center:[ -70.57880159991507,-33.59846466294888],
+      zoom:16,
+      collectResourceTiming: false,
+    });
+  }
+  viajes:Viaje[]=[]
+  idPasajero=localStorage.getItem('idUsuario')
+
+  ngOnInit() {
+    if (localStorage.getItem('perfil')==='chofer') {
+      this.navCtrl.navigateRoot('home-chofer')
+    }
+    this.listar();
+  }
+
+
+listar() {
+  this.crudViaje.listar().subscribe(data => {
+      this.viajes = data;
+      this.viajes.forEach((viaje) => {
+        this.crudChofer.getChofer(viaje.chofer).subscribe( dataChofer =>{
+          viaje.chofer= dataChofer.nombre+' '+dataChofer.apellido
+        })
+      });
+    },
+    error => {
+      console.error('Error al obtener los viajes:', error);
+    }
+  );
+}
 }
